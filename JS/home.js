1c66810903443f002c80228c56bf99ffcc2a2c38
[rami Dannah] <[killx51@hotmail.com]>
2019-05-27 21:01:54 +0300

    add few elements

--- JS/home.js
+++ JS/home.js
@@ -34,35 +34,91 @@ var div5_node =document.createElement("div");
 div5_node.setAttribute("class","col");
 div2_node.appendChild(div5_node);
 
+/*
+
+icons
+
+*/
+
+// mail
+//create div with class row
+var div6_node =document.createElement("div");
+div6_node.setAttribute("class","row ");
+div3_node.appendChild(div6_node);
 //create i with class mail
-var i1_node =document.createElement("i");
-div3_node.setAttribute("class","fas fa-envelope-open");
-div3_node.appendChild(i1_node);
+var i1_node = document.createElement("i");
+i1_node.setAttribute("class","fa fa-envelope fa-5x");
+div6_node.appendChild(i1_node);
+//style
+div6_node.style.backgroundColor = "#EDD4ED";
+div6_node.style.width = "1140px";
+div6_node.style.position = "absolute";
 
+
+
+//loction
+//create div with class row
+var div7_node =document.createElement("div");
+div7_node.setAttribute("class","row");
+div3_node.appendChild(div7_node);
 //create i with class loction
-var i2_node =document.createElement("i");
-//div3_node.setAttribute("class","col");
-div3_node.appendChild(i2_node);
+var i2_node = document.createElement("i");
+i2_node.setAttribute("class","fa fa-location-arrow fa-5x");
+div7_node.appendChild(i2_node);
+//style
+div7_node.style.backgroundColor = "#EEBFED";
+div7_node.style.width = "1140px";
+div7_node.style.position = "absolute";
+div7_node.style.top = "240px";
 
+
+//phone
+//create div with class row
+var div8_node =document.createElement("div");
+div8_node.setAttribute("class","row");
+div3_node.appendChild(div8_node);
 //create i with class phone
-var i3_node =document.createElement("i");
-//div3_node.setAttribute("class","col");
-div3_node.appendChild(i3_node);
+var i3_node = document.createElement("i");
+i3_node.setAttribute("class","fa fa-phone fa-5x");
+div8_node.appendChild(i3_node);
+//style
+div8_node.style.backgroundColor = "#89B0D8";
+div8_node.style.width = "1140px";
+div8_node.style.position = "absolute";
+div8_node.style.top = "160px";
 
+//twitter
+//create div with class row
+var div9_node =document.createElement("div");
+div9_node.setAttribute("class","row");
+div3_node.appendChild(div9_node);
+// create i with class twitter
+var i4_node = document.createElement("i");
+i4_node.setAttribute("class","fab fa-twitter fa-5x");
+div9_node.appendChild(i4_node);
+//style
+div9_node.style.backgroundColor = "#BAE8ED";
+div9_node.style.width = "1140px";
+div9_node.style.position = "absolute";
+div9_node.style.top = "80px";
 
-//create i with class twitter
-var i4_node =document.createElement("i");
-//div3_node.setAttribute("class","col");
-div3_node.appendChild(i4_node);
+/*
 
+info box
 
-
-
-
-
-
-
-
-
-
-
+*/
+//box
+var div10_node = document.createElement("div");
+div4_node.appendChild(div10_node);
+//style
+div10_node.style.backgroundColor = "#EAEBEB";
+div10_node.style.marginLeft = "-303px";
+div10_node.style.marginRight = "80px";
+div10_node.style.marginBottom = "50px";
+//h1
+var h1_node = document.createElement("h1");
+h1_node.setAttribute("title" , "proflie name");
+h1_node.innerText  = "rami \n dannah";
+div10_node.appendChild(h1_node);
+//style 
+//h1_node.style.fontFamily = "";
